8d82d460b283119c3399a215665767521a6fe62d
Eduke@gmai.com <alwin@gmail.com>
2026-05-17 15:11:49 +0530

    count

--- Eduke/eduke/main/context_processors.py
+++ Eduke/eduke/main/context_processors.py
@@ -3,25 +3,25 @@ from django.db import connection
 def total_unread_messages(request):
     user_id = None
     
-    if 'class_id' in request.session:
+    if '/class_head/' in request.path and 'class_id' in request.session:
         with connection.cursor() as cursor:
             cursor.execute("SELECT user_id FROM main_classes WHERE id = %s", [request.session['class_id']])
             row = cursor.fetchone()
             if row: user_id = row[0]
             
-    elif 'subject_id' in request.session:
+    elif '/subject_head/' in request.path and 'subject_id' in request.session:
         with connection.cursor() as cursor:
             cursor.execute("SELECT user_id FROM main_subjects WHERE id = %s", [request.session['subject_id']])
             row = cursor.fetchone()
             if row: user_id = row[0]
             
-    elif 'student_id' in request.session:
+    elif '/student/' in request.path and 'student_id' in request.session:
         with connection.cursor() as cursor:
             cursor.execute("SELECT user_id FROM main_students WHERE id = %s", [request.session['student_id']])
             row = cursor.fetchone()
             if row: user_id = row[0]
             
-    elif 'parent_id' in request.session:
+    elif '/parent/' in request.path and 'parent_id' in request.session:
         with connection.cursor() as cursor:
             cursor.execute("SELECT user_id FROM main_parents WHERE id = %s", [request.session['parent_id']])
             row = cursor.fetchone()
